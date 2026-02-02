dac4ab28fd4000b7eac9843fba15a137f153361e
RottenOtter14111 <arogers@anamosa.k12.ia.us>
2026-02-02 02:47:51 +0000

    done

--- Calendar.cs
+++ Calendar.cs
@@ -1,9 +1,11 @@
-﻿namespace knightmoves;
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace knightmoves;
 public class Calendar
 {
     public string[] Seasons = {"Summer", "Fall", "Winter", "Spring"};
 
     public string FindCurrentSeason(){
-        return ""; // Add your code here
+        return Seasons[1];
     }
 }
